e5e5eb436650bc44e6e106a329b224a9b4911613
Leticia-Mendiola <lmendiola509@gmail.com>
2021-03-01 12:46:23 -0600

    made current date/ time look fancier

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"))
-// $("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
+$("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
