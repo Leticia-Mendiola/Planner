c66cd8c4a6cc9e8af6e8d0259e818491daa99b38
Leticia-Mendiola <lmendiola509@gmail.com>
2021-03-01 19:19:35 -0600

    started piecing together localStorage code

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
+
+var saveBtn = $('#saveBtn')
+// var textarea = $('#textarea')
+
+saveBtn.on("click", function() {
+    localStorage.setItem("textarea", textarea);
+})
+
+function renderLast() {
+    localStorage.getItem("textarea")
+}
+
+renderLast()
