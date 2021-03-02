2b9f83f661fb703ea8aa31291c6f03c9d74a9e28
Leticia-Mendiola <lmendiola509@gmail.com>
2021-03-01 21:23:14 -0600

    continued debuging local storage functions

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
 
 var saveBtn = $('#saveBtn')
-// var textarea = $('#textarea')
+var textarea1 = $('#textarea1')
+var comment = textarea1.value.trim()
+
+function init() {
+    
+}
+
+function storeComments() {
+localStorage.setItem("textarea1", JSON.stringify(textarea1.value.trim()));
+}
 
 saveBtn.on("click", function(event) {
     event.preventDefault();
-    localStorage.setItem("textarea", textarea);
-})
+    storeComments();
+});
 
-function renderLast(event) {
-    event.preventDefault();
-    localStorage.getItem("textarea");
+function renderLast() {
+    var lastSave = JSON.parse(localStorage.getItem("textarea1"));
+    if (lastSave !== null) {
+        textarea1.text = lastSave;
+    }
 }
 
-renderLast()
+renderLast();
