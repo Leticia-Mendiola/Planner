27d8f6cd380b1bafdb0302e8f87fb3641f12e164
Leticia-Mendiola <lmendiola509@gmail.com>
2021-03-01 22:43:00 -0600

    still trying to debug local storage and attribute handling

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
 
-var saveBtn = $('#saveBtn')
+var currentHour = today.format("HH")
+var future = $(".future");
+var present = $(".present");
+var past = $(".past");
+var hourID = $('input[data-hour="8"]');
+
+if (hourID == currentHour) {
+    future.hide();
+    present.show();
+    past.hide();
+};
+if (hourID > currentHour) {
+    future.show();
+    present.hide();
+    past.hide()
+}
+if (hourID < currentHour) {
+    future.hide();
+    present.hide();
+    past.show();
+}
+
+
+
+var saveBtn = $('.saveBtn')
 var textarea1 = $('#textarea1')
 var comment = textarea1.value.trim()
 
-function init() {
-    
-}
-
 function storeComments() {
-localStorage.setItem("textarea1", JSON.stringify(textarea1.value.trim()));
+localStorage.setItem("#textarea1", JSON.stringify(textarea1.value.trim()));
 }
 
-saveBtn.on("click", function(event) {
+$(".saveBtn").on("click", function(event) {
     event.preventDefault();
     storeComments();
 });
@@ -21,7 +41,7 @@ saveBtn.on("click", function(event) {
 function renderLast() {
     var lastSave = JSON.parse(localStorage.getItem("textarea1"));
     if (lastSave !== null) {
-        textarea1.text = lastSave;
+        $("#textarea1").text(lastSave);
     }
 }
 
